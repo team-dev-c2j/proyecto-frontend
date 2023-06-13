ff90aa90512e3136c7a0d045503c8ad890f3ec02
porlanagustin <porlan.agustin@gmail.com>
2023-06-13 19:04:28 -0300

    breve modificacion

--- src/componentes/Products.jsx
+++ src/componentes/Products.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
 function ProductsComponent() {
   const [users, setUsers] = useState([]);
@@ -7,16 +7,16 @@ function ProductsComponent() {
     // Función para obtener los usuarios
     const fetchUsers = async () => {
       try {
-        const response = await fetch('http://127.0.0.1:8080/products');
+        const response = await fetch("http://127.0.0.1:3000/products");
         const data = await response.json();
         if (response.ok) {
           setUsers(data.results);
-          console.log('ok')
+          console.log("ok");
         } else {
           console.error(data.error);
         }
       } catch (error) {
-        console.error('Error fetching users:', error);
+        console.error("Error fetching users:", error);
       }
     };
 
@@ -33,10 +33,11 @@ function ProductsComponent() {
         <ul>
           {users.map((user) => (
             <div>
-                <li key={user.id}>{user.id}</li>
-                <li key={user.name}>{user.name}</li>
+              <li key={user._id}>{user._id}</li>
+              <li key={user.title}>{user.title}</li>
+              <li key={user.size}>{user.size}</li>
+              <li key={user.stock}>{user.stock}</li>
             </div>
-
           ))}
         </ul>
       )}
