910b1c5921f54a523092c3081cc65a0ea7af0d41
julimax <julimax1600@gmail.com>
2023-06-29 14:21:59 -0300

    panelAdmin update marcas

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import Login from './componentes/Login';
 import Admin from './componentes/Admin';
 import BodyCard from './componentes/BodyCard';
 import './styles/index.css';
+import EditMarcas from './componentes/adminComponentes/EditcMarcas';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -16,6 +17,7 @@ ReactDOM.render(
         <Route path="/" element={<BodyCard />} />
         <Route path="/login" element={<Login />} />
         <Route path="/admin" element={<Admin />} />
+        <Route path="/marcas/:id" element={ <EditMarcas/> } />
       </Routes>
       <Footer />
     </Router>
