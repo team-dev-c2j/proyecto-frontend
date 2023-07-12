c74e16c27d150c40282f8686e49bb0bf5a303f7c
julimax <julimax1600@gmail.com>
2023-07-12 19:16:49 -0300

    carrit

--- src/componentes/NavBar.jsx
+++ src/componentes/NavBar.jsx
@@ -9,7 +9,9 @@ import { Link } from 'react-router-dom';
 function Navbar () {
     return (
         <div className='barra1'>
-            <h2 className='name'>KZ</h2>
+            <Link to={'/'}>
+                <h2 className='name'>KZ</h2>
+            </Link>
             <div className='sector2'>
             <Link>
                 <h6 className='items'>New</h6> </Link>
@@ -18,8 +20,11 @@ function Navbar () {
             </Link>
             <Link to="/admin">
                 <h6 className='items'>Admin</h6>
-            </Link>            
-            <button type="button" id='botoninfo' class="btn btn-outline-info">Carro</button>
+            </Link>     
+            <Link to="/carrito">
+            <button type="button" id='botoninfo' class="btn btn-outline-info">Carro</button>    
+            </Link>       
+
             </div>
 
 
